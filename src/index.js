48d071a4c8e190970cf30867b971fb396e97bf31
Andrii Telishko <andrii.telishko@valtech.com>
2021-09-17 16:52:57 +0300

    start setup webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import helloFn from './helloFn';
+import './styles/styles.css';
+import './styles/scss.scss';
+
+console.log('Hello world');
+
+helloFn();
---
+++ webpack.config.js
@@ -0,0 +1,153 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+
+const devMode = process.env.NODE_ENV !== "production";
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (!devMode) {
+        config.minimizer = [
+            new OptimizeCssAssetPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config;
+};
+
+const cssLoader = (extra) => {
+    const loaders = [devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader']
+    
+    if (extra) {
+        loaders.push(extra)
+    }
+    return loaders
+};
+
+const jsLoaders = () => {
+    const loaders = [{
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/preset-env']
+        }
+    }]
+
+    if (devMode) {
+        loaders.push('eslint-loader')
+    }
+}
+
+module.exports = {
+    mode: 'development',
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    optimization: optimization(),
+
+    devServer: {
+        port: 4004
+    },
+    
+    devtool: devMode ? 'source-map' : 'nosources-source-map',
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: !devMode 
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css' 
+        }),
+        
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoader()
+            },
+            {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          
+            'file-loader',
+            {
+      loader: 'image-webpack-loader',
+      options: {
+        mozjpeg: {
+          progressive: true,
+        },
+        // optipng.enabled: false will disable optipng
+        optipng: {
+          enabled: false,
+        },
+        pngquant: {
+          quality: [0.65, 0.90],
+          speed: 4
+        },
+        gifsicle: {
+          interlaced: false,
+        },
+        // the webp option will enable WEBP
+        webp: {
+          quality: 75
+        }
+      }
+    },
+              
+          
+        ],
+      },
+    ],
+            },
+            {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+        
+      
+           {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoader('sass-loader')
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                }
+            }
+            },
+        ]
+    }
+}
