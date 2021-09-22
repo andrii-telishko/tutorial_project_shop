b97034bf4b71318b650726fc9ee2732a7d6cc26c
Andrii Telishko <andrii.telishko@valtech.com>
2021-09-22 20:24:42 +0300

    finish mobile ver

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+/* eslint-disable no-undef */
 import helloFn from './helloFn';
 import './styles/main.scss';
+import 'normalize.css';
+import './index.html'
 
+    const menuBtnRef = document.querySelector("[data-menu-button]")
+    const mobileMenuRef = document.querySelector("[data-menu]")
+
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+        mobileMenuRef.classList.toggle("is-open");
+    });
 
 
 console.log('Hello world');
