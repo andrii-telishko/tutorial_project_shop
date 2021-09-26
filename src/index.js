3141ca6eb55e0c2a088c72c541491fa786b70445
Andrii Telishko <andrii.telishko@valtech.com>
2021-09-26 20:24:17 +0300

    start doing validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './js/slider';
-import './js/radio-checked';
+// import './js/radio-checked';
+// import './js/formValidation';
 import './styles/main.scss';
 import 'normalize.css';
+
+const wrapper = document.querySelectorAll('.checkout-form__group');
 
 const menuBtnRef = document.querySelector('[data-menu-button]');
 const mobileMenuRef = document.querySelector('[data-menu]');
@@ -13,4 +16,20 @@ menuBtnRef.addEventListener('click', () => {
   menuBtnRef.setAttribute('aria-expanded', !expanded);
 
   mobileMenuRef.classList.toggle('is-open');
+});
+
+wrapper[1].addEventListener('click', e => {
+  if (e.target.value === 'credit_card') {
+    card.classList.remove('visually-hidden');
+    transfer.classList.add('visually-hidden');
+    paypal.classList.add('visually-hidden');
+  } else if (e.target.value === 'transfer') {
+    card.classList.add('visually-hidden');
+    transfer.classList.remove('visually-hidden');
+    paypal.classList.add('visually-hidden');
+  } else if (e.target.value === 'paypal') {
+    card.classList.add('visually-hidden');
+    transfer.classList.add('visually-hidden');
+    paypal.classList.remove('visually-hidden');
+  }
 });
--- src/js/radio-checked.js
+++ src/js/radio-checked.js
@@ -1,21 +1,21 @@
-const radioBtn = document.querySelectorAll('.radio-btn');
-const card = document.querySelector('.payment-card');
-const transfer = document.querySelector('.payment-bank');
-const paypal = document.querySelector('.payment-paypal');
-const wrapper = document.querySelectorAll('.checkout-form__group');
+// const radioBtn = document.querySelectorAll('.radio-btn');
+// const card = document.querySelector('.payment-card');
+// const transfer = document.querySelector('.payment-bank');
+// const paypal = document.querySelector('.payment-paypal');
+// const wrapper = document.querySelectorAll('.checkout-form__group');
 
-wrapper[1].addEventListener('click', e => {
-  if (e.target.value === 'credit_card') {
-    card.classList.remove('visually-hidden');
-    transfer.classList.add('visually-hidden');
-    paypal.classList.add('visually-hidden');
-  } else if (e.target.value === 'transfer') {
-    card.classList.add('visually-hidden');
-    transfer.classList.remove('visually-hidden');
-    paypal.classList.add('visually-hidden');
-  } else if (e.target.value === 'paypal') {
-    card.classList.add('visually-hidden');
-    transfer.classList.add('visually-hidden');
-    paypal.classList.remove('visually-hidden');
-  }
-});
+// wrapper[1].addEventListener('click', e => {
+//   if (e.target.value === 'credit_card') {
+//     card.classList.remove('visually-hidden');
+//     transfer.classList.add('visually-hidden');
+//     paypal.classList.add('visually-hidden');
+//   } else if (e.target.value === 'transfer') {
+//     card.classList.add('visually-hidden');
+//     transfer.classList.remove('visually-hidden');
+//     paypal.classList.add('visually-hidden');
+//   } else if (e.target.value === 'paypal') {
+//     card.classList.add('visually-hidden');
+//     transfer.classList.add('visually-hidden');
+//     paypal.classList.remove('visually-hidden');
+//   }
+// });
