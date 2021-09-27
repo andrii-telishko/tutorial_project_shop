7d9639700fb7fd53ef7fb8c210b5efe805c679c5
Andrii Telishko <andrii.telishko@valtech.com>
2021-09-27 15:26:39 +0300

    finish validation part

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
 import './js/slider';
-// import './js/radio-checked';
+// import './radio-checked';
 // import './js/formValidation';
 import './styles/main.scss';
 import 'normalize.css';
+// import './js/burger';
 
-const wrapper = document.querySelectorAll('.checkout-form__group');
+// const radioBtn = document.querySelectorAll('.radio-btn');
+// const card = document.querySelector('.payment-card');
+// const transfer = document.querySelector('.payment-bank');
+// const paypal = document.querySelector('.payment-paypal');
+// const wrapper = document.querySelectorAll('.checkout-form__group');
+
+// const form = document.querySelector('.checkout-form');
+// // const card = document.querySelector('.payment-card');
+// const select = document.querySelectorAll('.checkout-form__select');
+// const buyerInput = document.querySelectorAll('.buyer-input');
+// const cardInput = document.querySelectorAll('.card-input');
+// const checkbox = document.querySelectorAll('.checkbox');
+// const error = document.querySelector('.error');
+// const btn = document.querySelector('.checkout-form__submit-btn');
 
 const menuBtnRef = document.querySelector('[data-menu-button]');
 const mobileMenuRef = document.querySelector('[data-menu]');
+
+// wrapper[1].addEventListener('click', e => {
+//   if (e.target.value === 'credit_card') {
+//     card.classList.remove('visually-hidden');
+//     transfer.classList.add('visually-hidden');
+//     paypal.classList.add('visually-hidden');
+//   } else if (e.target.value === 'transfer') {
+//     card.classList.add('visually-hidden');
+//     transfer.classList.remove('visually-hidden');
+//     paypal.classList.add('visually-hidden');
+//   } else if (e.target.value === 'paypal') {
+//     card.classList.add('visually-hidden');
+//     transfer.classList.add('visually-hidden');
+//     paypal.classList.remove('visually-hidden');
+//   }
+// });
+
+// form.addEventListener('change', () => {
+//   const invalidInputBuyer = [...buyerInput].find(item => !item.validity.valid);
+//   const invalidInputCard = [...cardInput].find(item => !item.validity.valid);
+
+//   if (!invalidInputBuyer && !invalidInputCard) {
+//     btn.removeAttribute('disabled');
+//   } else {
+//     btn.setAttribute('disabled', true);
+//   }
+// });
 
 menuBtnRef.addEventListener('click', () => {
   const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
@@ -16,20 +57,4 @@ menuBtnRef.addEventListener('click', () => {
   menuBtnRef.setAttribute('aria-expanded', !expanded);
 
   mobileMenuRef.classList.toggle('is-open');
-});
-
-wrapper[1].addEventListener('click', e => {
-  if (e.target.value === 'credit_card') {
-    card.classList.remove('visually-hidden');
-    transfer.classList.add('visually-hidden');
-    paypal.classList.add('visually-hidden');
-  } else if (e.target.value === 'transfer') {
-    card.classList.add('visually-hidden');
-    transfer.classList.remove('visually-hidden');
-    paypal.classList.add('visually-hidden');
-  } else if (e.target.value === 'paypal') {
-    card.classList.add('visually-hidden');
-    transfer.classList.add('visually-hidden');
-    paypal.classList.remove('visually-hidden');
-  }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,153 +4,158 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
+const loader = require('sass-loader');
 
-
-
-const devMode = process.env.NODE_ENV !== "production";
+const devMode = process.env.NODE_ENV !== 'production';
 
 const optimization = () => {
-    const config = {
-        splitChunks: {
-            chunks: 'all'
-        }
-    }
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
 
-    if (!devMode) {
-        config.minimizer = [
-            new OptimizeCssAssetPlugin(),
-            new TerserWebpackPlugin()
-        ]
-    }
-    return config;
+  if (!devMode) {
+    config.minimizer = [
+      new OptimizeCssAssetPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+  return config;
 };
 
-const cssLoader = (extra) => {
-    const loaders = [devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-        'css-loader']
-    
-    if (extra) {
-        loaders.push(extra)
-    }
-    return loaders
+const cssLoader = extra => {
+  const loaders = [
+    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+    'css-loader',
+  ];
+
+  if (extra) {
+    loaders.push(extra);
+  }
+  return loaders;
 };
 
 const jsLoaders = () => {
-    const loaders = [{
-        loader: 'babel-loader',
-        options: {
-            presets: ['@babel/preset-env']
-        }
-    }]
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ];
 
-    if (devMode) {
-        loaders.push('eslint-loader')
-    }
-    return loaders;
-}
+  if (devMode) {
+    loaders.push('eslint-loader');
+  }
+  return loaders;
+};
 
 module.exports = {
-    mode: 'development',
-    entry: ['@babel/polyfill', './src/index.js'],
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
+  mode: 'development',
+  entry: {
+    index: ['@babel/polyfill', './src/index.js'],
+    checkout: ['@babel/polyfill', './src/checkout.js'],
+  },
+  output: {
+    filename: 'js/[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-    optimization: optimization(),
+  optimization: optimization(),
 
-    devServer: {
-        port: 4004
-    },
-    
-    devtool: devMode ? 'source-map' : 'nosources-source-map',
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            minify: {
-                collapseWhitespace: !devMode
-            }
-        }),
-        new HTMLWebpackPlugin({
-            template: './src/checkout.html',
-            filename: 'checkout.html',
-            minify: {
-                collapseWhitespace: !devMode
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        })
-          
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: cssLoader()
-            },
-            {
-                test: /\.(ttf|woff|woff2|eot)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: cssLoader('sass-loader')
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: jsLoaders()
-                },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                use: [
-    {
-      loader: 'image-webpack-loader',
-      options: {
-        mozjpeg: {
-          progressive: true,
-        },
-        // optipng.enabled: false will disable optipng
-        optipng: {
-          enabled: false,
-        },
-        pngquant: {
-          quality: [0.65, 0.90],
-          speed: 4
-        },
-        gifsicle: {
-          interlaced: false,
-        },
-        // the webp option will enable WEBP
-        webp: {
-          quality: 75
-        }
-      }
-    },
+  devServer: {
+    port: 4004,
+  },
+
+  devtool: devMode ? 'source-map' : 'nosources-source-map',
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['index'],
+      minify: {
+        collapseWhitespace: !devMode,
+      },
+    }),
+    new HTMLWebpackPlugin({
+      template: './src/checkout.html',
+      filename: 'checkout.html',
+      chunks: ['checkout'],
+      minify: {
+        collapseWhitespace: !devMode,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+    }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoader(),
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoader('sass-loader'),
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
             },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            },
-            // {
-            //     test: /\.html$/,
-            //     use: [
-            //         {
-            //             loader: 'file-loader',
-            //             options: {
-            //                 name: '[name].[ext]'
-            //             }
-            //         }
-            //     ],
-            //     exclude: path.resolve(__dirname, 'src/index.html')
-            // }
-            
-
-        ]
-    }
-}
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      // {
+      //     test: /\.html$/,
+      //     use: [
+      //         {
+      //             loader: 'file-loader',
+      //             options: {
+      //                 name: '[name].[ext]'
+      //             }
+      //         }
+      //     ],
+      //     exclude: path.resolve(__dirname, 'src/index.html')
+      // }
+    ],
+  },
+};
