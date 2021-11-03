55a9c2d2352cff1c3b65823a5278d9c6374dd867
andrii-telishko <telishkoav@gmail.com>
2021-11-03 20:23:56 +0200

    last commit

--- src/js/cart/renderCreditPrice.js
+++ src/js/cart/renderCreditPrice.js
@@ -3,19 +3,23 @@ import refs from '../common/refs';
 const renderCreditPrice = () => {
   const checkedInput = [...refs.bankInputs].find(input => input.checked);
 
-  const value = JSON.parse(checkedInput.value);
+  if (checkedInput) {
+    const value = JSON.parse(checkedInput.value);
 
-  refs.creditPrice.textContent = `$${value.total}`;
+    refs.creditPrice.textContent = `$${value.total}`;
 
-  refs.paymentPrice.textContent = `$${value.credit}`;
+    refs.paymentPrice.textContent = `$${value.credit}`;
 
-  refs.cartTotalPriceOnPage.setAttribute('style', 'color: grey');
+    refs.cartTotalPriceOnPage.setAttribute('style', 'color: grey');
 
-  refs.creditPriceBlock.setAttribute('style', 'display: grid');
+    refs.creditPriceBlock.setAttribute('style', 'display: grid');
 
-  refs.paymentPriceBlock.setAttribute('style', 'display: grid');
+    refs.paymentPriceBlock.setAttribute('style', 'display: grid');
 
-  refs.modalBackdrop.classList.add('is-hidden');
+    refs.modalBackdrop.classList.add('is-hidden');
+  } else {
+    return;
+  }
 };
 
 export default renderCreditPrice;
